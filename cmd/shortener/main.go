3eb46cbbea7686b40212cd44c572bd37974ad8ae
Dmitry <mazaevdmitry@mail.ru>
2023-01-27 11:33:29 +0300

    iter1

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,15 @@
 package main
 
-func main() {}
+import (
+	"github.com/mazaevdmitry/go-yandex-shortener/internal/app"
+	"log"
+)
+
+func main() {
+	server := app.Server()
+	log.Println("Starting server...")
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
